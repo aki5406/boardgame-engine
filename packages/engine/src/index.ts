d41c70c4b1730f77aeb5a658d88b976b1bb7d2c1
aki5406 <51012126+aki5406@users.noreply.github.com>
2026-06-30 19:07:08 +0900

    Introduce Engine concept

--- packages/engine/src/index.ts
+++ packages/engine/src/index.ts
@@ -1,4 +1,4 @@
-export type { Engine } from "./engine.js";
+export type { Engine, EngineApplyEventInput, EngineStartSessionInput } from "./engine.js";
 export type { EngineEvent } from "./event.js";
 export type { EngineState } from "./state.js";
 export type { EngineReducer } from "./reducer.js";
